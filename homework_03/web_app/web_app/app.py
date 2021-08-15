d28083d8f1f389fa17206ce6ac98bcd2f4e73dc4
romulandiy <romulandiy@gmail.com>
2021-08-15 17:56:56 +0300

    updated # 1

--- homework_03/web_app/web_app/app.py
+++ homework_03/web_app/web_app/app.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 
 @app.get("/", summary='Get a hello world json')
-def read_root():
-    return {"Hello": "World 3234424"}
+def get_root(name:str = 'World!',):
+    """
+    Hello World!
+    1. processes requests
+    1. return greeting
+    """
+    return {"Hello": name}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
