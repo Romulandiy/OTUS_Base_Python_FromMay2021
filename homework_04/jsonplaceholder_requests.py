760f1041158c5d43a1166a974aeee5cbd8615a1f
romulandiy <romulandiy@gmail.com>
2021-09-24 22:52:52 +0300

    updated #2 homework_4

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -2,5 +2,40 @@
 создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
 """
 
-USERS_DATA_URL = ""
-POSTS_DATA_URL = ""
+from dataclasses import dataclass
+
+from aiohttp import ClientSession
+from loguru import logger
+
+USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
+POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
+
+
+
+@dataclass
+class Service:
+    name: str
+    url: str
+    field: str
+
+
+SERVICES = [
+    Service("ipify", "https://api.ipify.org/?format=json", "ip"),
+    Service("ip-api", "http://ip-api.com/json", "query"),
+]
+
+
+async def fetch_json(session: ClientSession, url: str) -> dict:
+    async with session.get(url) as response:
+        return await response.json()
+
+
+async def fetch_ip(service: Service) -> str:
+    logger.info("Fetch ip from {}", service.name)
+
+    async with ClientSession() as session:
+        result = await fetch_json(session, service.url)
+
+    logger.info("Fetched json from {!r}: {}", service.name, result)
+
+    return result.get(service.field)
