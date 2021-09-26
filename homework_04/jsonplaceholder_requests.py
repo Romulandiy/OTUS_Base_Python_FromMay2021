19824e5167d7273e44c720e90f96bf7252775e7b
romulandiy <romulandiy@gmail.com>
2021-09-26 18:57:58 +0300

    updated #3 homework_4

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -1,27 +1,27 @@
 """
 создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
 """
-
+import asyncio
 from dataclasses import dataclass
 
 from aiohttp import ClientSession
 from loguru import logger
-
-USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users"
-POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts"
-
 
 
 @dataclass
 class Service:
     name: str
     url: str
-    field: str
+    fields: list
 
 
+USERS_DATA_URL = 'https://jsonplaceholder.typicode.com/users'
+POSTS_DATA_URL = 'https://jsonplaceholder.typicode.com/posts'
+
+# SERVICES = [USERS_DATA_URL, POSTS_DATA_URL]
 SERVICES = [
-    Service("ipify", "https://api.ipify.org/?format=json", "ip"),
-    Service("ip-api", "http://ip-api.com/json", "query"),
+    Service('json_users', USERS_DATA_URL, ['name', 'username', 'email']),
+    Service('json_posts', POSTS_DATA_URL, ['user_id', 'title', 'body']),
 ]
 
 
@@ -30,12 +30,66 @@ async def fetch_json(session: ClientSession, url: str) -> dict:
         return await response.json()
 
 
-async def fetch_ip(service: Service) -> str:
-    logger.info("Fetch ip from {}", service.name)
+async def fetch_fields(service: SERVICES):
+    logger.info("Fetch from {}", service.name)
 
     async with ClientSession() as session:
         result = await fetch_json(session, service.url)
 
-    logger.info("Fetched json from {!r}: {}", service.name, result)
+    logger.info("Fetched json {}", result)
 
-    return result.get(service.field)
+    return result
+
+
+async def async_main():
+    for i in SERVICES:
+        await fetch_fields(i)
+
+
+def main():
+    asyncio.run(async_main())
+
+
+if __name__ == '__main__':
+    main()
+
+# @dataclass
+# class Service:
+#     name: str
+#     url: str
+#     field: str
+#
+#
+# SERVICES = [
+#     Service("ipify", "https://api.ipify.org/?format=json", "ip"),
+#     Service("ip-api", "http://ip-api.com/json", "query"),
+# ]
+#
+#
+# async def fetch_json(session: ClientSession, url: str) -> dict:
+#     async with session.get(url) as response:
+#         return await response.json()
+#
+#
+# async def fetch_ip(service: Service) -> str:
+#     logger.info("Fetch ip from {}", service.name)
+#
+#     async with ClientSession() as session:
+#         result = await fetch_json(session, service.url)
+#
+#     logger.info("Fetched json from {!r}: {}", service.name, result)
+#
+#     return result.get(service.field)
+#
+#
+# async def async_main():
+#     for service in SERVICES:
+#         await fetch_ip(service)
+#
+#
+# def main():
+#     asyncio.run(async_main())
+#
+#
+# if __name__ == '__main__':
+#     main()
