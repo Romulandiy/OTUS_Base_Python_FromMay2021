41b312119e9d5d68cf38025dba53b2a356c78170
romulandiy <romulandiy@gmail.com>
2021-09-26 22:32:17 +0300

    updated #4 homework_4

--- homework_04/models.py
+++ homework_04/models.py
@@ -8,9 +8,53 @@
 создайте связи relationship между моделями: User.posts и Post.user
 """
 
-import os
+# import os
+#
+# from sqlalchemy.ext.asyncio import create_async_engine
+#
+# PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
+#
+# Base = None
+# Session = None
+#
+#
+# engine = create_async_engine()
 
-PG_CONN_URI = os.environ.get("SQLALCHEMY_PG_CONN_URI") or "postgresql+asyncpg://postgres:password@localhost/postgres"
 
-Base = None
-Session = None
+
+
+from datetime import datetime
+
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    String,
+    Boolean,
+    DateTime,
+    func,
+)
+from sqlalchemy.orm import declarative_base
+
+engine = create_engine("sqlite:///example-db-03.sqlite", echo=True)
+
+
+Base = declarative_base(bind=engine)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), unique=True)
+    is_staff = Column(Boolean, nullable=False, default=False)
+    created_at = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+        server_default=func.now(),
+    )
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all()
